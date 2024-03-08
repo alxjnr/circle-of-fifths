5929f7f6c20d7467b6fcf0df0d452d1d7b2bd77e
alex <alexrjoiner@gmail.com>
2024-03-08 20:56:00 +0000

    add quiz functionality

--- src/data/Questions.jsx
+++ src/data/Questions.jsx
@@ -1,6 +1,407 @@
 export const questions = [
   {
-    question: "hello how are you?",
-    answers: ["good", "fine", "great"],
+    question: "ii of C",
+    answers: ["Am", "Fm", "Gm"],
+    correct_answer: "Am",
+  },
+  {
+    question: "iii of C",
+    answers: ["Am", "Em", "Bm"],
+    correct_answer: "Em",
+  },
+  {
+    question: "IV of C",
+    answers: ["F", "G", "A"],
+    correct_answer: "F",
+  },
+  {
+    question: "V of C",
+    answers: ["G", "C", "D"],
+    correct_answer: "G",
+  },
+  {
+    question: "vi of C",
+    answers: ["Am", "Dm", "Em"],
+    correct_answer: "Am",
+  },
+  {
+    question: "vii° of C",
+    answers: ["Bdim", "Fdim", "Gdim"],
+    correct_answer: "Bdim",
+  },
+  {
+    question: "ii of G",
+    answers: ["Am", "Em", "Bm"],
+    correct_answer: "Am",
+  },
+  {
+    question: "iii of G",
+    answers: ["Bm", "F#m", "Em"],
+    correct_answer: "Bm",
+  },
+  {
+    question: "IV of G",
+    answers: ["C", "D", "E"],
+    correct_answer: "C",
+  },
+  {
+    question: "V of G",
+    answers: ["D", "A", "E"],
+    correct_answer: "D",
+  },
+  {
+    question: "vi of G",
+    answers: ["Em", "Am", "Bm"],
+    correct_answer: "Em",
+  },
+  {
+    question: "vii° of G",
+    answers: ["F#dim", "C#dim", "D#dim"],
+    correct_answer: "F#dim",
+  },
+  {
+    question: "ii of D",
+    answers: ["Em", "Bm", "F#m"],
+    correct_answer: "Em",
+  },
+  {
+    question: "iii of D",
+    answers: ["F#m", "C#m", "G#m"],
+    correct_answer: "F#m",
+  },
+  {
+    question: "IV of D",
+    answers: ["G", "A", "B"],
+    correct_answer: "G",
+  },
+  {
+    question: "V of D",
+    answers: ["A", "E", "B"],
+    correct_answer: "A",
+  },
+  {
+    question: "vi of D",
+    answers: ["Bm", "Em", "F#m"],
+    correct_answer: "Bm",
+  },
+  {
+    question: "vii° of D",
+    answers: ["C#dim", "G#dim", "A#dim"],
+    correct_answer: "C#dim",
+  },
+  {
+    question: "I of A",
+    answers: ["A", "Bm", "C#m"],
+    correct_answer: "A",
+  },
+  {
+    question: "ii of A",
+    answers: ["Bm", "C#m", "D"],
+    correct_answer: "Bm",
+  },
+  {
+    question: "iii of A",
+    answers: ["C#m", "D", "E"],
+    correct_answer: "C#m",
+  },
+  {
+    question: "IV of A",
+    answers: ["D", "E", "F#m"],
+    correct_answer: "D",
+  },
+  {
+    question: "V of A",
+    answers: ["E", "F#m", "G#dim"],
+    correct_answer: "E",
+  },
+  {
+    question: "vi of A",
+    answers: ["F#m", "G#dim", "A"],
+    correct_answer: "F#m",
+  },
+  {
+    question: "vii° of A",
+    answers: ["G#dim", "A", "Bm"],
+    correct_answer: "G#dim",
+  },
+  {
+    question: "I of E",
+    answers: ["E", "F#m", "G#m"],
+    correct_answer: "E",
+  },
+  {
+    question: "ii of E",
+    answers: ["F#m", "G#m", "A"],
+    correct_answer: "F#m",
+  },
+  {
+    question: "iii of E",
+    answers: ["G#m", "A", "B"],
+    correct_answer: "G#m",
+  },
+  {
+    question: "IV of E",
+    answers: ["A", "B", "C#m"],
+    correct_answer: "A",
+  },
+  {
+    question: "V of E",
+    answers: ["B", "C#m", "D#dim"],
+    correct_answer: "B",
+  },
+  {
+    question: "vi of E",
+    answers: ["C#m", "D#dim", "E"],
+    correct_answer: "C#m",
+  },
+  {
+    question: "vii° of E",
+    answers: ["D#dim", "E", "F#m"],
+    correct_answer: "D#dim",
+  },
+  {
+    question: "I of B",
+    answers: ["B", "C#m", "D#m"],
+    correct_answer: "B",
+  },
+  {
+    question: "ii of B",
+    answers: ["C#m", "D#m", "E"],
+    correct_answer: "C#m",
+  },
+  {
+    question: "iii of B",
+    answers: ["D#m", "E", "F#"],
+    correct_answer: "D#m",
+  },
+  {
+    question: "IV of B",
+    answers: ["E", "F#", "G#m"],
+    correct_answer: "E",
+  },
+  {
+    question: "V of B",
+    answers: ["F#", "G#m", "A#dim"],
+    correct_answer: "F#",
+  },
+  {
+    question: "vi of B",
+    answers: ["G#m", "A#dim", "B"],
+    correct_answer: "G#m",
+  },
+  {
+    question: "vii° of B",
+    answers: ["A#dim", "B", "C#m"],
+    correct_answer: "A#dim",
+  },
+  {
+    question: "I of Gb",
+    answers: ["Gb", "Abm", "Bbm"],
+    correct_answer: "Gb",
+  },
+  {
+    question: "ii of Gb",
+    answers: ["Abm", "Bbm", "Cb"],
+    correct_answer: "Abm",
+  },
+  {
+    question: "iii of Gb",
+    answers: ["Bbm", "Cb", "Db"],
+    correct_answer: "Bbm",
+  },
+  {
+    question: "IV of Gb",
+    answers: ["Cb", "Db", "Ebm"],
+    correct_answer: "Cb",
+  },
+  {
+    question: "V of Gb",
+    answers: ["Db", "Ebm", "Fdim"],
+    correct_answer: "Db",
+  },
+  {
+    question: "vi of Gb",
+    answers: ["Ebm", "Fdim", "Gb"],
+    correct_answer: "Ebm",
+  },
+  {
+    question: "vii° of Gb",
+    answers: ["Fdim", "Gb", "Abm"],
+    correct_answer: "Fdim",
+  },
+  {
+    question: "I of Db",
+    answers: ["Db", "Ebm", "Fm"],
+    correct_answer: "Db",
+  },
+  {
+    question: "ii of Db",
+    answers: ["Ebm", "Fm", "Gb"],
+    correct_answer: "Ebm",
+  },
+  {
+    question: "iii of Db",
+    answers: ["Fm", "Gb", "Ab"],
+    correct_answer: "Fm",
+  },
+  {
+    question: "IV of Db",
+    answers: ["Gb", "Ab", "Bbm"],
+    correct_answer: "Gb",
+  },
+  {
+    question: "V of Db",
+    answers: ["Ab", "Bbm", "Cdim"],
+    correct_answer: "Ab",
+  },
+  {
+    question: "vi of Db",
+    answers: ["Bbm", "Cdim", "Db"],
+    correct_answer: "Bbm",
+  },
+  {
+    question: "vii° of Db",
+    answers: ["Cdim", "Db", "Ebm"],
+    correct_answer: "Cdim",
+  },
+  {
+    question: "I of Ab",
+    answers: ["Ab", "Bbm", "Cm"],
+    correct_answer: "Ab",
+  },
+  {
+    question: "ii of Ab",
+    answers: ["Bbm", "Cm", "Db"],
+    correct_answer: "Bbm",
+  },
+  {
+    question: "iii of Ab",
+    answers: ["Cm", "Db", "Eb"],
+    correct_answer: "Cm",
+  },
+  {
+    question: "IV of Ab",
+    answers: ["Db", "Eb", "Fm"],
+    correct_answer: "Db",
+  },
+  {
+    question: "V of Ab",
+    answers: ["Eb", "Fm", "Gdim"],
+    correct_answer: "Eb",
+  },
+  {
+    question: "vi of Ab",
+    answers: ["Fm", "Gdim", "Ab"],
+    correct_answer: "Fm",
+  },
+  {
+    question: "vii° of Ab",
+    answers: ["Gdim", "Ab", "Bbm"],
+    correct_answer: "Gdim",
+  },
+  {
+    question: "I of Eb",
+    answers: ["Eb", "Fm", "Gm"],
+    correct_answer: "Eb",
+  },
+  {
+    question: "ii of Eb",
+    answers: ["Fm", "Gm", "Ab"],
+    correct_answer: "Fm",
+  },
+  {
+    question: "iii of Eb",
+    answers: ["Gm", "Ab", "Bb"],
+    correct_answer: "Gm",
+  },
+  {
+    question: "IV of Eb",
+    answers: ["Ab", "Bb", "Cm"],
+    correct_answer: "Ab",
+  },
+  {
+    question: "V of Eb",
+    answers: ["Bb", "Cm", "Ddim"],
+    correct_answer: "Bb",
+  },
+  {
+    question: "vi of Eb",
+    answers: ["Cm", "Ddim", "Eb"],
+    correct_answer: "Cm",
+  },
+  {
+    question: "vii° of Eb",
+    answers: ["Ddim", "Eb", "Fm"],
+    correct_answer: "Ddim",
+  },
+  {
+    question: "I of Bb",
+    answers: ["Bb", "Cm", "Dm"],
+    correct_answer: "Bb",
+  },
+  {
+    question: "ii of Bb",
+    answers: ["Cm", "Dm", "Eb"],
+    correct_answer: "Cm",
+  },
+  {
+    question: "iii of Bb",
+    answers: ["Dm", "Eb", "F"],
+    correct_answer: "Dm",
+  },
+  {
+    question: "IV of Bb",
+    answers: ["Eb", "F", "Gm"],
+    correct_answer: "Eb",
+  },
+  {
+    question: "V of Bb",
+    answers: ["F", "Gm", "Adim"],
+    correct_answer: "F",
+  },
+  {
+    question: "vi of Bb",
+    answers: ["Gm", "Adim", "Bb"],
+    correct_answer: "Gm",
+  },
+  {
+    question: "vii° of Bb",
+    answers: ["Adim", "Bb", "Cm"],
+    correct_answer: "Adim",
+  },
+  {
+    question: "I of F",
+    answers: ["F", "Gm", "Am"],
+    correct_answer: "F",
+  },
+  {
+    question: "ii of F",
+    answers: ["Gm", "Am", "Bb"],
+    correct_answer: "Gm",
+  },
+  {
+    question: "iii of F",
+    answers: ["Am", "Bb", "C"],
+    correct_answer: "Am",
+  },
+  {
+    question: "IV of F",
+    answers: ["Bb", "C", "Dm"],
+    correct_answer: "Bb",
+  },
+  {
+    question: "V of F",
+    answers: ["C", "Dm", "Edim"],
+    correct_answer: "C",
+  },
+  {
+    question: "vi of F",
+    answers: ["Dm", "Edim", "F"],
+    correct_answer: "Dm",
+  },
+  {
+    question: "vii° of F",
+    answers: ["Edim", "F", "Gm"],
+    correct_answer: "Edim",
   },
 ];
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -4,26 +4,72 @@ import { questions } from "../data/Questions";
 
 export default function Quiz() {
   const [questionsArr, setQuestionsArr] = useState(questions);
+  const [index, setIndex] = useState(0);
+  const [points, setPoints] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [scoreLabel, setScoreLabel] = useState("");
+
+  function checkAnswer(answer) {
+    if (questionsArr[index].correct_answer == answer) {
+      setPoints((prev) => prev + 1);
+    }
+    setIndex((prev) => prev + 1);
+    if (index >= 14) setIsPlaying(false);
+    setScoreLabel(`You scored ${points}/15`);
+  }
+
+  function beginQuiz() {
+    setPoints(0);
+    setScoreLabel("");
+    setIndex(0);
+
+    const shuffledQuestions = [...questionsArr].map((question) => {
+      let shuffledAnswers = [...question.answers];
+      for (let i = shuffledAnswers.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledAnswers[i], shuffledAnswers[j]] = [
+          shuffledAnswers[j],
+          shuffledAnswers[i],
+        ];
+      }
+      return { ...question, answers: shuffledAnswers };
+    });
+
+    for (let i = shuffledQuestions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledQuestions[i], shuffledQuestions[j]] = [
+        shuffledQuestions[j],
+        shuffledQuestions[i],
+      ];
+    }
+
+    setQuestionsArr(shuffledQuestions);
+    setIsPlaying(true);
+  }
 
   return (
     <div>
       <Navbar />
       <div className="main-centered">
         <div className="quiz-container">
-          {questionsArr.map((e) => {
-            return (
-              <div>
-                <div>
-                  <h2>{e.question}</h2>
-                </div>
-                <div className="quiz-answer-container">
-                  {e.answers.map((i) => {
-                    return <button>{i}</button>;
-                  })}
-                </div>
+          {isPlaying ? (
+            <div>
+              <div className="quiz-question-wrapper">
+                <h2>{questionsArr[index].question}</h2>
               </div>
-            );
-          })}
+              <div className="quiz-answer-container">
+                {questionsArr[index].answers.map((e) => {
+                  return <button onClick={() => checkAnswer(e)}>{e}</button>;
+                })}
+              </div>
+              <h4>{points}/15</h4>
+            </div>
+          ) : (
+            <div>
+              <button onClick={beginQuiz}>Start Quiz</button>
+              <h3>{scoreLabel}</h3>
+            </div>
+          )}
         </div>
       </div>
     </div>
