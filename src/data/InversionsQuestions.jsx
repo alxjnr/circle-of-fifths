7f84bf466dc98f7300faee1ae6ca72f874d32372
alex <alexrjoiner@gmail.com>
2024-03-20 09:42:11 +0000

    add inversion questions

--- src/data/InversionsQuestions.jsx
+++ src/data/InversionsQuestions.jsx
@@ -29,4 +29,74 @@ export const questions = [
     answers: ["Emaj", "Bbmin", "Dbmaj"],
     correct_answer: "Dbmaj",
   },
+  {
+    question: ["Ab3", "Db/C#4", "E4"],
+    answers: ["Fmin", "Dbmin", "Bmin"],
+    correct_answer: "Dbmin",
+  },
+  {
+    question: ["Ab3", "Db/C#4", "E3"],
+    answers: ["Dbmin", "Ebmin", "Amaj"],
+    correct_answer: "Dbmin",
+  },
+  {
+    question: ["A3", "D4", "Gb/F#4"],
+    answers: ["Cmaj", "Ebmaj", "Dmaj"],
+    correct_answer: "Dmaj",
+  },
+  {
+    question: ["A3", "D4", "Gb/F#3"],
+    answers: ["Cmin", "Dmaj", "Dbmaj"],
+    correct_answer: "Dmaj",
+  },
+  {
+    question: ["A3", "D4", "F4"],
+    answers: ["Dmin", "Amin", "Cmaj"],
+    correct_answer: "Dmin",
+  },
+  {
+    question: ["A3", "D4", "F3"],
+    answers: ["Emin", "Dmin", "Gb/F#Maj"],
+    correct_answer: "Dmin",
+  },
+  {
+    question: ["Bb3", "Eb4", "G4"],
+    answers: ["Dbmaj", "Ebmaj", "Abmin"],
+    correct_answer: "Ebmaj",
+  },
+  {
+    question: ["Bb3", "Eb4", "G3"],
+    answers: ["Bbmin", "Ebmin", "Ebmaj"],
+    correct_answer: "Ebmaj",
+  },
+  {
+    question: ["Bb3", "Eb4", "Gb/F#4"],
+    answers: ["Abmin", "Ebmin", "Gb/F#min"],
+    correct_answer: "Ebmin",
+  },
+  {
+    question: ["Bb3", "Eb4", "Gb/F#3"],
+    answers: ["Ebmin", "Dbmin", "Bmaj"],
+    correct_answer: "Ebmin",
+  },
+  {
+    question: ["F4", "A4", "C3"],
+    answers: ["Fmaj", "Cmaj", "Amin"],
+    correct_answer: "Fmaj",
+  },
+  {
+    question: ["F4", "A3", "C3"],
+    answers: ["Ebmin", "Emin", "Fmaj"],
+    correct_answer: "Fmaj",
+  },
+  {
+    question: ["F4", "Ab4", "C3"],
+    answers: ["Emaj", "Amaj", "Fmin"],
+    correct_answer: "Fmin",
+  },
+  {
+    question: ["F4", "Ab3", "C3"],
+    answers: ["Ebmin", "Fmin", "Gb/F#maj"],
+    correct_answer: "Fmin",
+  },
 ];
