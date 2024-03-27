37d87385b408e0aa3dd4e50b7a63ab3241ffcb3a
alex <alexrjoiner@gmail.com>
2024-03-27 11:47:53 +0000

    begin implementing circle explorer functionality

--- src/pages/CircleExplorer.jsx
+++ src/pages/CircleExplorer.jsx
@@ -1,4 +1,75 @@
-function Circle({ top, left, width, height, text }) {
+import { useState } from "react";
+import { CircleExplorerData } from "../data/CircleExplorerData";
+import { useEffect } from "react";
+
+function Circle({
+  top,
+  left,
+  size,
+  colour,
+  text,
+  isBordered,
+  isSelected,
+  setExplorerCircles,
+}) {
+  function selectNote() {
+    setExplorerCircles((prev) => {
+      const arr = [...prev];
+      arr.forEach((e) => {
+        e.is_selected = false;
+        e.is_bordered = false;
+      });
+      const noteSelected = arr.filter((e) => e.note_name == text);
+      arr.forEach((e) => {
+        if (e.note_name == noteSelected[0].note_name) e.is_bordered = true;
+        noteSelected[0].diatonic_chords.forEach((i) => {
+          if (e.note_name == i) {
+            e.is_selected = true;
+            e.note_colour = "lightblue";
+          }
+        });
+        noteSelected[0].borrowed_chords.forEach((i) => {
+          if (e.note_name == i) {
+            e.is_selected = true;
+            e.note_colour = "lightgreen";
+          }
+        });
+        noteSelected[0].parallel_notes.forEach((i) => {
+          if (e.note_name == i) {
+            e.is_selected = true;
+            e.note_colour = "yellow";
+          }
+        });
+        if (e.note_name == noteSelected[0].dominant_chord) {
+          e.is_selected = true;
+          e.note_colour = "linear-gradient(lightblue  0 75%, coral 75% 100%)";
+        }
+        if (e.note_name == noteSelected[0].subdominant_chord) {
+          e.is_selected = true;
+          e.note_colour =
+            "linear-gradient(lightblue 0 75%,  darkmagenta 75% 100%)";
+        }
+        if (e.note_name == noteSelected[0].relative_note) {
+          e.is_selected = true;
+          e.note_colour = "linear-gradient(lightblue 0 75%, deeppink 75% 100%)";
+        }
+        if (e.note_name == noteSelected[0].secondary_dominant_chord) {
+          e.is_selected = true;
+          e.note_colour = "forestgreen";
+        }
+        if (e.note_name == noteSelected[0].leading_tone) {
+          e.is_selected = true;
+          e.note_colour = "linear-gradient(lightgreen 0 75%, khaki 75% 100%)";
+        }
+        if (e.note_name == noteSelected[0].tritone_sub) {
+          e.is_selected = true;
+          e.note_colour = "steelblue";
+        }
+      });
+      return arr;
+    });
+  }
+
   return (
     <div
       className="circle-explorer-circles"
@@ -6,10 +77,12 @@ function Circle({ top, left, width, height, text }) {
         position: "absolute",
         top: `calc(50% + ${top}%)`,
         left: `calc(50% + ${left}%)`,
-        width: `${width}%`,
-        height: `${height}%`,
+        width: `${size == 0 ? 10 : 7}%`,
+        height: `${size == 0 ? 10 : 7}%`,
         borderRadius: "50%",
-        border: "solid black 1px",
+        border: `solid ${isBordered ? "red" : "gray"} ${
+          isBordered ? "2px" : "1px"
+        }`,
         transformOrigin: "center",
         transform: "translate(-50%, -50%)",
         zIndex: 2,
@@ -18,8 +91,10 @@ function Circle({ top, left, width, height, text }) {
         alignItems: "center",
         justifyContent: "center",
         zIndex: 0,
-        backgroundColor: "white",
+        background: isSelected ? colour : "white",
+        cursor: "pointer",
       }}
+      onClick={selectNote}
     >
       <p>{text}</p>
     </div>
@@ -29,18 +104,18 @@ function Circle({ top, left, width, height, text }) {
 function Legend({ colour, text }) {
   return (
     <div
+      className="legend-divs"
       style={{
         display: "flex",
         flexDirection: "row",
         justifyContent: "flex-start",
-        gap: "10px",
         alignItems: "center",
-        width: "90px",
+        gap: "10%",
       }}
     >
       <div
         style={{
-          width: "10px",
+          width: "10%",
           height: "10px",
           backgroundColor: colour,
         }}
@@ -51,95 +126,48 @@ function Legend({ colour, text }) {
 }
 
 export default function CircleExplorer() {
+  const [explorerCircles, setExplorerCircles] = useState(CircleExplorerData);
+
   return (
     <div>
       <div className="main-centered">
         <div className="circle-explorer-container">
-          <div
-            style={{
-              position: "relative",
-              border: "solid black 1px",
-              height: "80%",
-              width: "73%",
-              borderRadius: "50%",
-            }}
-          >
-            <Circle top={-40} left={0} width={10} height={10} text={"C"} />
-            <Circle top={40} left={0} width={10} height={10} text={"Gb/F#"} />
-            <Circle top={0} left={40} width={10} height={10} text={"A"} />
-            <Circle top={0} left={-40} width={10} height={10} text={"Eb"} />
-            <Circle top={-35} left={-20} width={10} height={10} text={"F"} />
-            <Circle top={-20} left={-35} width={10} height={10} text={"Bb"} />
-            <Circle top={35} left={20} width={10} height={10} text={"B/Cb"} />
-            <Circle top={20} left={35} width={10} height={10} text={"E"} />
-            <Circle top={35} left={-20} width={10} height={10} text={"Db/C#"} />
-            <Circle top={20} left={-35} width={10} height={10} text={"Ab"} />
-            <Circle top={-35} left={20} width={10} height={10} text={"G"} />
-            <Circle top={-20} left={35} width={10} height={10} text={"D"} />
-            <Circle top={-40 + 15} left={0} width={7} height={7} text={"Am"} />
-            <Circle top={40 - 15} left={0} width={7} height={7} text={"Ebm"} />
-            <Circle top={0} left={40 - 15} width={7} height={7} text={"F#m"} />
-            <Circle top={0} left={-40 + 15} width={7} height={7} text={"Cm"} />
-            <Circle
-              top={-35 + 13}
-              left={-20 + 6}
-              width={7}
-              height={7}
-              text={"Dm"}
-            />
-            <Circle
-              top={-20 + 7}
-              left={-35 + 12}
-              width={7}
-              height={7}
-              text={"Gm"}
-            />
-            <Circle
-              top={35 - 13}
-              left={20 - 6}
-              width={7}
-              height={7}
-              text={"G#m"}
-            />
-            <Circle
-              top={20 - 7}
-              left={35 - 12}
-              width={7}
-              height={7}
-              text={"C#m"}
-            />
-            <Circle
-              top={35 - 13}
-              left={-20 + 6}
-              width={7}
-              height={7}
-              text={"Bbm"}
-            />
-            <Circle
-              top={20 - 7}
-              left={-35 + 12}
-              width={7}
-              height={7}
-              text={"Fm"}
-            />
-            <Circle
-              top={-35 + 13}
-              left={20 - 6}
-              width={7}
-              height={7}
-              text={"Em"}
-            />
-            <Circle
-              top={-20 + 7}
-              left={35 - 12}
-              width={7}
-              height={7}
-              text={"Bm"}
-            />
+          <div className="circle-explorer-main-circle">
+            <div
+              style={{
+                position: "relative",
+                border: "solid gray 1px",
+                height: "80%",
+                width: "73%",
+                borderRadius: "50%",
+              }}
+            >
+              {explorerCircles.map((e) => {
+                return (
+                  <Circle
+                    top={e.note_top}
+                    left={e.note_left}
+                    size={e.note_size}
+                    colour={e.note_colour}
+                    text={e.note_name}
+                    isSelected={e.is_selected}
+                    isBordered={e.is_bordered}
+                    setExplorerCircles={setExplorerCircles}
+                  />
+                );
+              })}
+            </div>
           </div>
           <div className="circle-explorer-legend">
-            <Legend colour={"blue"} text={"Diatonic"} />
-            <Legend colour={"red"} text={"Borrowed"} />
+            <Legend colour={"lightblue"} text={"Diatonic Chords"} />
+            <Legend colour={"lightgreen"} text={"Borrowed Chords"} />
+            <Legend colour={"yellow"} text={"Parallel Major/Minor"} />
+            <Legend colour={"coral"} text={"Dominant"} />
+            <Legend colour={"darkmagenta"} text={"Subdominant"} />
+            <Legend colour={"deeppink"} text={"Relative Major/Minor"} />
+            <Legend colour={"forestgreen"} text={"Secondary Dominant"} />
+            <Legend colour={"khaki"} text={"Leading Tone"} />
+            <Legend colour={"steelblue"} text={"Tritone Substitution"} />
           </div>
         </div>
       </div>
